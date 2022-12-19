12d4826999072db406228d9d3d5814d45ceb930c
Ditto <bearibeiromonteir@gmail.com>
2022-12-19 19:38:41 +0000

    no se

---
+++ src/Servidor/Servidor.java
@@ -0,0 +1,25 @@
+package Servidor;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Servidor {
+    /*
+        Método que conecta o Servidor aos Clientes e inicializa os sockets para a comunicação
+
+        @param   args          Argumentos passados aquando da inicialização
+        @throws  IOException   Exceção lançada caso algo inesperado aconteça
+     */
+    public static void main(String[] args) throws IOException{
+        ServerSocket ss = new ServerSocket(12345);
+        Map<String,Utilizador> utilizadores = new HashMap<>();
+
+        while(true){
+            Socket cs = ss.accept();
+
+        }
+    }
+}
